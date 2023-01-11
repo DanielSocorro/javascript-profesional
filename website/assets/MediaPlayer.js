9234abc5c3471b22bd1b2412c77095950577395f
Daniel Socorro <daniel.socorro.c@gmail.com>
2023-01-10 21:25:40 -0300

    fixing mediaplayer

--- website/assets/MediaPlayer.js
+++ website/assets/MediaPlayer.js
@@ -1,45 +1,47 @@
-function MediaPlayer(config) {
-    this.media = config.el;
-    this.plugins = config.plugins || [];
-    
-    this._initPlugins();
+class MediaPlayer {
+    constructor(config) {
+        this.media = config.el;
+        this.plugins = config.plugins || [];
 
-  }
+        this._initPlugins();
 
-  MediaPlayer.prototype._initPlugins = function () {
-    const player = {
-        play: () => this.play(),
-        pause: () => this.pause(),
-        media: this.media,
-        get muted() {
-            return this.media.muted;
-        },
+    }
+    _initPlugins() {
+        const player = {
+            play: () => this.play(),
+            pause: () => this.pause(),
+            media: this.media,
+            get muted() {
+                return this.media.muted;
+            },
 
-        set muted(value) {
-            this.media.muted = value;
-        },
-    };
+            set muted(value) {
+                this.media.muted = value;
+            },
+        };
 
 
-    this.plugins.forEach(plugin => {
-        plugin.run(this);
-    });
-  };
+        this.plugins.forEach(plugin => {
+            plugin.run(this);
+        });
+    }
+    play() {
+        this.media.play();
+    }
+    pause() {
+        this.media.pause();
+    }
+    mute() {
+        this.media.muted = true;
+    }
+    unmute() {
+        this.media.muted = false;
+    }
+}
 
-  MediaPlayer.prototype.play = function() {
-      this.media.play();
-  }
+
   
-  MediaPlayer.prototype.pause = function() {
-      this.media.pause();
-  }
   
-  MediaPlayer.prototype.mute = function() {
-      this.media.muted = true;
-  }
   
-  MediaPlayer.prototype.unmute = function() {
-      this.media.muted = false;
-  }
 
   export default MediaPlayer;
